efb03e6891a6df5e707e4b0f2785a22df668e959
UditKamdar <63956159+UditKamdar@users.noreply.github.com>
2021-04-24 18:21:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,79 @@
 import numpy as np
 import pickle
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request ,render_template
 from flask_cors import CORS, cross_origin
 
+#import libraries
 
+
+#Initialize the flask App
 app = Flask(__name__)
 CORS(app)
 maxT = pickle.load(open('forecast_model_maxT.pkl', 'rb'))
 minT = pickle.load(open('forecast_model_minT.pkl', 'rb'))
+
 print("working")
-# with open('forecast_model_maxT.pkl', 'rb') as fin:
-#     m2 = pickle.load(fin)
-@app.route("/")
-def trial():
-    return <h1>hello </h1>
-@app.route("agriculture-assistance.herokuapp.com/")
-def predict1():
-    # horizon = int(request.json['horizon'])
-    
+
+#default page of our web-app
+@app.route('/')
+def home():
+    print("working good")
+    return render_template('index.html',prediction_text='The second batting team will win the match')
+
+@app.route('https://agriculture-assistance.herokuapp.com/')
+def home1():
+    print("working good")
+    return render_template('index.html',prediction_text='Hello')
+
+#To use the predict button in our web-app
+# @app.route('/predict',methods=['POST'])
+# def predict():
+#     '''
+#     For rendering results on HTML GUI
+#     '''
+#     print("Reached")
 #     future2 = maxT.make_future_dataframe(periods=365)
 #     forecast2 = maxT.predict(future2)
+#     # output = round(prediction[0], 2)
     
-#     data = forecast2[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head()
+#     print(output)
+#     #  return render_template('index.html', prediction_text='The second batting team will  :{}'.format(output))
     
-#     # data = forecast2[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
-#     ret = data.to_json(orient='records', date_format='iso')
-#     print(ret)
-    
-    return <h1>hey</h1>
-# running REST interface, port=3000 for direct test
-
-@app.route('agriculture-assistance.herokuapp.com/api',methods=['GET'])
-def predict():
-    d={}
-    d['Query'] = str(request.args['Query'])
-    
-    return jsonify(d)
-
+#     return render_template('index.html', prediction_text='The second batting team will win the match')
+   
+         
 if __name__ == "__main__":
     app.run(debug=True)
+
+# app = Flask(__name__)
+# CORS(app)
+# maxT = pickle.load(open('forecast_model_maxT.pkl', 'rb'))
+# minT = pickle.load(open('forecast_model_minT.pkl', 'rb'))
+# print("working")
+# # with open('forecast_model_maxT.pkl', 'rb') as fin:
+# #     m2 = pickle.load(fin)
+# @app.route("/")
+# def predict1():
+#     # horizon = int(request.json['horizon'])
+    
+#     # future2 = maxT.make_future_dataframe(periods=365)
+#     # forecast2 = maxT.predict(future2)
+    
+#     # data = forecast2[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head()
+    
+#     # # data = forecast2[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+#     # ret = data.to_json(orient='records', date_format='iso')
+#     # print(ret)
+    
+#     return ret
+# # running REST interface, port=3000 for direct test
+
+# @app.route('/agriculture-assistance.herokuapp.com/api',methods=['GET'])
+# def predict():
+#     d={}
+#     d['Query'] = str(request.args['Query'])
+    
+#     return jsonify(d)
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=3000)
